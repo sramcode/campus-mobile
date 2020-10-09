1b21817bbece5737c8a3c14e0748828932b13fde
sramcode <sram@ucsd.edu>
2020-10-09 16:24:53 -0700

    added seperate midterm view, issues with routing

--- lib/ui/views/class_schedule/midterm_list.dart
+++ lib/ui/views/class_schedule/midterm_list.dart
@@ -10,112 +10,154 @@ import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 class MidtermList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<SectionData> midtermData =
+        Provider.of<ClassScheduleDataProvider>(context).midterms;
     return ContainerView(
-//      child: buildSchedule(context),
-        );
+      child: buildMidtermsSchedule(midtermData, context),
+    );
+  }
+
+  String abbrevToFullWeekday(String abbreviation) {
+    switch (abbreviation) {
+      case 'MO':
+        return 'Monday';
+      case 'TU':
+        return 'Tuesday';
+      case 'WE':
+        return 'Wednesday';
+      case 'TH':
+        return 'Thursday';
+      case 'FR':
+        return 'Friday';
+      case 'SA':
+        return 'Saturday';
+      case 'SU':
+        return 'Sunday';
+      case 'Other':
+        return 'Other';
+      default:
+        return 'Other';
+    }
+  }
+
+  Widget buildMidtermsSchedule(
+      List<SectionData> midtermData, BuildContext context) {
+    List<Widget> listToReturn = List<Widget>();
+
+    for (SectionData data in midtermData) {
+      listToReturn.add(ListTile(
+        title: buildClassTitle(
+            data.subjectCode + ' ' + data.courseCode, data.courseTitle),
+        subtitle: Column(
+          children: [
+            buildDateRow(data.days, data.date),
+            buildTimeRow(data.time),
+            buildLocationRow(data.building + ' ' + data.room),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+      ));
+    }
+    listToReturn =
+        ListTile.divideTiles(tiles: listToReturn, context: context).toList();
+    listToReturn.add(
+      Padding(
+        padding: const EdgeInsets.only(left: 16.0, bottom: 8.0),
+      ),
+    );
+    return ListView(
+      // TODO: Resolve #709
+      // Re-enable scrolling if content overflows space
+//        physics: NeverScrollableScrollPhysics(),
+      children: listToReturn,
+    );
+  }
+
+  String formatDate(String date) {
+    String month = date.substring(5, 7);
+    String day = date.substring(8);
+    switch (month) {
+      case "01":
+        month = "January";
+        break;
+      case "02":
+        month = "February";
+        break;
+      case "03":
+        month = "March";
+        break;
+      case "04":
+        month = "April";
+        break;
+      case "05":
+        month = "May";
+        break;
+      case "06":
+        month = "June";
+        break;
+      case "07":
+        month = "July";
+        break;
+      case "08":
+        month = "August";
+        break;
+      case "09":
+        month = "September";
+        break;
+      case "10":
+        month = "October";
+        break;
+      case "11":
+        month = "November";
+        break;
+      case "12":
+        month = "December";
+        break;
+    }
+
+    return month + " " + day;
+  }
+
+  Widget buildDateRow(String day, String date) {
+    String weekday = abbrevToFullWeekday(day);
+    String monthDate = formatDate(date);
+
+    return Text(weekday + "," + monthDate);
+  }
+
+  Widget buildClassTitle(String title, String courseName) {
+    return Text(title + ": " + courseName);
+  }
+
+  Widget buildTimeRow(String time) {
+    return Row(
+      children: <Widget>[
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TimeRangeWidget(
+                time: time,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildLocationRow(String location) {
+    return Row(
+      children: <Widget>[
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(location),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
-//
-//  Widget buildSchedule(BuildContext context) {
-//    List<Widget> list = List<Widget>();
-//    Provider.of<ClassScheduleDataProvider>(context).midterms.keys.forEach(
-//      (key) {
-//        if (Provider.of<ClassScheduleDataProvider>(context)
-//            .midterms[key]
-//            .isNotEmpty) {
-//          list.add(SliverStickyHeader(
-//            header: buildWeekDayHeader(context, key),
-//            sliver: SliverList(
-//              delegate: SliverChildBuilderDelegate(
-//                  (context, index) => buildClass(
-//                      Provider.of<ClassScheduleDataProvider>(context)
-//                          .enrolledClasses[key]
-//                          .elementAt(index)),
-//                  childCount: Provider.of<ClassScheduleDataProvider>(context)
-//                      .enrolledClasses[key]
-//                      .length),
-//            ),
-//          ));
-//        }
-//      },
-//    );
-//    return CustomScrollView(slivers: list);
-//  }
-//
-//  Widget buildWeekDayHeader(BuildContext context, String weekday) {
-//    weekday = abbrevToFullWeekday(weekday);
-//    return Container(
-//      color: Theme.of(context).secondaryHeaderColor,
-//      child: Padding(
-//        padding: const EdgeInsets.all(12.0),
-//        child: Text(
-//          weekday,
-//          style: TextStyle(
-//            fontSize: 20.0,
-//            color: Colors.white,
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//
-//  Widget buildClass(SectionData sectionData) {
-//    return sectionData != null
-//        ? Card(
-//            child: Padding(
-//              padding: const EdgeInsets.all(8.0),
-//              child: Column(
-//                crossAxisAlignment: CrossAxisAlignment.start,
-//                children: <Widget>[
-//                  Text(
-//                    sectionData.subjectCode + ' ' + sectionData.courseCode,
-//                    style:
-//                        TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
-//                  ),
-//                  Text(
-//                    sectionData.courseTitle,
-//                    style: TextStyle(fontSize: 17.0),
-//                  ),
-//                  Text(sectionData.instructorName,
-//                      style: TextStyle(fontSize: 17.0)),
-//                  Padding(
-//                    padding: const EdgeInsets.only(top: 5.0),
-//                    child: Row(children: [
-//                      Text(sectionData.meetingType + ' '),
-//                      TimeRangeWidget(
-//                        time: sectionData.time,
-//                      )
-//                    ]),
-//                  ),
-//                  Text(sectionData.building + ' ' + sectionData.room),
-//                  Text(sectionData.gradeOption)
-//                ],
-//              ),
-//            ),
-//          )
-//        : null;
-//  }
-//
-//  String abbrevToFullWeekday(String abbreviation) {
-//    switch (abbreviation) {
-//      case 'MO':
-//        return 'Monday';
-//      case 'TU':
-//        return 'Tuesday';
-//      case 'WE':
-//        return 'Wednesday';
-//      case 'TH':
-//        return 'Thursday';
-//      case 'FR':
-//        return 'Friday';
-//      case 'SA':
-//        return 'Saturday';
-//      case 'SU':
-//        return 'Sunday';
-////      case 'Other':
-////        return 'Other';
-//      default:
-//        return 'Other';
-//    }
-//  }
-//}
